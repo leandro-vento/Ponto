3fcc8fd372bd6dbe46d8eca4418518f2f9cdf5fc
Leandro Vento <Leandro.Vento@fgv.br>
2020-08-19 03:41:05 -0300

    att

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -32,9 +32,9 @@ golem::add_module( name = "relatorios" ) # Name of the module
 
 # ## External resources
 # ## Creates .js and .css files at inst/app/www
-# golem::add_js_file( "script" )
-# golem::add_js_handler( "handlers" )
-# golem::add_css_file( "custom" )
+golem::add_js_file( "script" )
+golem::add_js_handler( "handlers" )
+golem::add_css_file( "custom" )
 
 # ## Add internal datasets ----
 # ## If you have data in your package
